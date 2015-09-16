33a7200b15fae60ae167b4e234e4cb3ca88795f9
Awdesh Sharma <awdesh.sharma1@gmail.com>
2015-09-15 23:06:26 -0500

    Two Strings are anagram.

---
+++ HomePractice/HomePractice/AnagramStrings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomePractice
+{
+    static class AnagramStrings
+    {
+        private const int NO_OF_CHARS = 256;
+        /// <summary>
+        /// Method verifies if two strings are anagram of each other or not.
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns>boolean value either true or false</returns>
+        public static bool AreStringsAnagram(string s1, string s2)
+        {
+            int[] count = new int[NO_OF_CHARS];
+
+            foreach(char c in s1.ToCharArray())
+            {
+                if(count[c] == 0)
+                    count[c]++;
+            }
+
+            foreach (char c in s2.ToCharArray())
+            {
+                if(count[c] == 1)
+                    count[c]--;
+            }
+
+            for(int i = 0; i< NO_OF_CHARS; i++)
+            {
+                if(count[i] != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
