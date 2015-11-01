da7d32fc2a144238fe157cda4aa63981ca1a8296
awdesh <awdesh.sharma1@gmail.com>
2015-11-01 13:01:58 -0600

    swapping nodes in a linked list.

---
+++ HomePractice/LinkedList/SwapNodes.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace LinkedList
+{
+	public class SwapNodes
+	{
+		public SwapNodes ()
+		{
+		}
+
+		public static LinkedListNode Swap(LinkedListNode n)
+		{
+			LinkedListNode buffer = null;
+			LinkedListNode newRoot = null;
+			while (n!=null && n.getNext()!=null) {
+				if (buffer != null) {
+					buffer.getNext ().setNext (n.getNext ());
+				}
+
+				buffer = n.getNext ();
+				n.setNext (n.getNext ().getNext ());
+
+				buffer.setNext (n);
+
+				if (newRoot == null) {
+					newRoot = buffer;
+				}
+
+				n = n.getNext ();
+			}
+
+			return newRoot;
+		}
+	}
+}
+
