d939a6f69609a5da3dc53c67c3bc391861037c59
Awdesh Sharma <awdesh.sharma1@gmail.com>
2015-11-14 14:00:58 -0600

    Duplicate code

--- HomePractice/MS/SolveStringArithmetic.cs
+++ HomePractice/MS/SolveStringArithmetic.cs
@@ -12,30 +12,30 @@ namespace MSSelection
 		/// Solve the specified input.
 		/// </summary>
 		/// <param name="input">Input.</param>
-		public int Solve(string input)
-		{
-			int length = input.Length;
-			int num = 0;
-			int i = 0;
-			bool isNeg = false;
+		// public int Solve(string input)
+		// {
+		// 	int length = input.Length;
+		// 	int num = 0;
+		// 	int i = 0;
+		// 	bool isNeg = false;
 
-			if (input [i] == '-') {
-				i++;
-				isNeg = true;
-			}
+		// 	if (input [i] == '-') {
+		// 		i++;
+		// 		isNeg = true;
+		// 	}
 
-			while (i<length) {
-				num = num * 10; // while thinking consider this as step-2.
-				num = num + (input [i] - '0'); // while thinking consider this as step- 1.
-				i++;
-			}
+		// 	while (i<length) {
+		// 		num = num * 10; // while thinking consider this as step-2.
+		// 		num = num + (input [i] - '0'); // while thinking consider this as step- 1.
+		// 		i++;
+		// 	}
 
-			if (isNeg) {
-				num *= -1;
-			}
+		// 	if (isNeg) {
+		// 		num *= -1;
+		// 	}
 
-			return num;
-		}
+		// 	return num;
+		// }
 	}
 }
 
